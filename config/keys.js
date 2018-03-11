e0861ebffd0955198188392ed83094122ce24382
Dennis Mullen <dkmullen@gmail.com>
2018-03-11 15:16:55 -0400

    Chase down err in config files

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,9 @@
 // If we are on heroku, this gets set to 'production'
 // Locally it probably isn't defined at all
 if (process.env.NODE_ENV === 'production') {
+  // we are in production - return the prod set of keys
   module.exports = require('./prod');
 } else {
+  // we are in development - return the dev keys!!!
   module.exports = require('./dev');
 }
